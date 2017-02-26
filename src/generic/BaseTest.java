64a0e82e6823e672b4b9f3bcb26a61565229571d
Ravisc <s.c.ravikumar@gmail.com>
2017-02-26 21:39:17 +0530

    test

---
+++ src/generic/BaseTest.java
@@ -0,0 +1,29 @@
+package generic;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public abstract class BaseTest implements Autoconst {
+	public WebDriver driver;
+	
+	@BeforeMethod
+	public void  precondition(){
+		System.setProperty(GECKO_KEY, GECKO_VALUE);
+		driver=new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.get("http://localhost/login.do");
+		
+	}
+	
+	@AfterMethod
+		public void postcondition(){
+			driver.quit();
+		}
+		
+	}
+
+
